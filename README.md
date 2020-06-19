2bb29ddaaac58b0c1348bdf47d1ef21d95f2fb0a
rong <1141591465@qq.com>
2020-06-19 19:42:36 +0800

    add more docs

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # golang supervisord learning
+
+
+## reload test 
+
+* build command
+
+```code
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
+```
+
+
+
+
+
+
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 	r.GET("/", func(context *gin.Context) {
-		context.String(200, "ok")
+		context.String(200, "ok demo")
 	})
 	r.Run(":8080")
 }
